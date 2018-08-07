4fee96366c9ee4fb262ae54bcc0f254874abcbee
Jack Branch <jackbranch@Jacks-MacBook-Pro.local>
2018-08-07 14:14:44 +0100

    added spec/src folders

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,13 @@
+"use strict";
+describe("Thermostat", function () {
+
+	var thermostat;
+
+  beforeEach(function () {
+    thermostat = new thermostat();
+  });
+
+  describe("Thermostat starts a 20 deg", function () {
+    
+  });
+});
